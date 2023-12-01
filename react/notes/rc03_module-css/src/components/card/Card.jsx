f6fee7426e9116921975cf65f31fd7ed65e479be
sevgi75 <erbilsevgi97@gmail.com>
2023-12-01 14:06:08 +0300

    data

--- react/notes/rc03_module-css/src/components/card/Card.jsx
+++ react/notes/rc03_module-css/src/components/card/Card.jsx
@@ -6,12 +6,12 @@ const Card = () => {
   // console.log(data);
   return (
     <div>
-      {data.map(({ language, img, btnName }) => (
-        <>
+      {data.map(({ id, language, img, btnName }) => (
+        <div key={id}>
           <h1>{language}</h1>
           <img src={img} alt="" />
           <Buton name={btnName} />
-        </>
+        </div>
       ))}
       {/* <h1>{data[0].language}</h1>
       <img src={data[0].img} alt="" />
