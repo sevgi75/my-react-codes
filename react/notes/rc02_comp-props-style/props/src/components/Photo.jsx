f4239ba25c0257069c2942cdfcc28e25b4742ade
sevgi75 <erbilsevgi97@gmail.com>
2023-12-01 12:17:01 +0300

    photo com

--- react/notes/rc02_comp-props-style/props/src/components/Photo.jsx
+++ react/notes/rc02_comp-props-style/props/src/components/Photo.jsx
@@ -1,7 +1,7 @@
-const Photo = () => {
+const Photo = ({ img }) => {
   return (
     <div>
-        <img src="https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373_640.jpg" alt="kid" />
+        <img src={img} alt="kid" />
     </div>
   )
 }
