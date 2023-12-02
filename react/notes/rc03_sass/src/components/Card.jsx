4781235a1826d6009b593323adfaaf30ca16497d
sevgi75 <erbilsevgi97@gmail.com>
2023-12-02 22:39:00 +0300

    card

--- react/notes/rc03_sass/src/components/Card.jsx
+++ react/notes/rc03_sass/src/components/Card.jsx
@@ -5,10 +5,11 @@ const Card = ({veri}) => {
   return (
     <div>
       {veri.map(({ id, name, job, comment, img}) => (
-        <div>
+        <div key={id}>
           <h2>{name}</h2>
           <h3>{job}</h3>
           <img src={img} alt="img" width={"100px"} />
+          <p>{comment}</p>
           <div>
             <button>Small</button>
             <button>Large</button>
