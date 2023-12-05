805c89916de2f9c317220be70d0fb7ec9038f679
sevgi75 <erbilsevgi97@gmail.com>
2023-12-05 22:29:13 +0300

    callback invoke

--- react/notes/rc05_events-useStateHook/src/1-events/Event.jsx
+++ react/notes/rc05_events-useStateHook/src/1-events/Event.jsx
@@ -9,10 +9,19 @@ const Event = () => {
         console.log("Name:", e.target.name);
         console.log("ID:", e.target.id);
     }
+
+    const handleSil = function (message) {
+        console.log("Silindi");
+        console.log(message);
+    }
+
+    const message = "FS15"
   return (
     <div>
         <h1>Events</h1>
         <button onClick={handleTikla} id="btn" name="my-button">Tikla</button>
+        {/* Eger bir event fonksiyonunun paremetresi olmasi gerekiyorsa bu fonksiyon bir arrow fonks. tarafindan  cagrilmalidir. Aksi takdirde event fonksiyonu event gerceklesmeden cagirilir */}
+        <button onClick={() => handleSil(message)}>Sil</button>
     </div>
   )
 }
