37e9da75093a5b2adca519e238be382a3be67c5e
sevgi75 <erbilsevgi97@gmail.com>
2023-12-06 20:04:14 +0300

    forms other events

---
+++ react/notes/rc06_forms-otherEvents/src/App.js
@@ -0,0 +1,15 @@
+import Form from "./components/1-forms/Form"
+import KeyboardClipboard from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
+import MouseEvent from "./components/3-mouseEvents/MouseEvent"
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <KeyboardClipboard />
+      <MouseEvent />
+    </div>
+  )
+}
+
+export default App
