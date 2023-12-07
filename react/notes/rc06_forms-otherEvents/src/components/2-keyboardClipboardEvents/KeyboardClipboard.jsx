324c14626956899ccdfcd7261cdee49034b1fd8c
sevgi75 <erbilsevgi97@gmail.com>
2023-12-08 01:06:45 +0300

    keyDown

--- react/notes/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ react/notes/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 
 const KeyboardClipboard = () => {
+  const [icerik, serIcerik] = useState("")
+
+  const handleKeyDown = (e) => {
+    // console.log(e.keyCode);
+    if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+      alert("Buraya rakam girilemez")
+      e.preventDefault() //? default davranisi kapatarak girilen rakamin inputa basilmasi engellenmis oldu
+    }
+    
+  }
   return (
-    <div>KeyboardClipboard</div>
+    <div className="container mt-4">
+      <h2>Keyboard Events</h2>
+
+      <input 
+      type="text" 
+      className="form-control"
+      onChange={(e) => serIcerik(e.target.value.toLocaleUpperCase())}
+      // HEP BUYUK HARF
+      value={icerik}
+      onKeyDown={handleKeyDown} 
+      />
+
+      <div className="mt-3">
+        {/*  hep kucuk harf */}
+        <p>{icerik.toLocaleLowerCase()}</p>
+      </div>
+    </div>
   )
 }
 
