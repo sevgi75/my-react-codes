dd5ce1341baa5b03b2c806093a2e0d1bc66a8795
sevgi75 <erbilsevgi97@gmail.com>
2023-12-07 14:37:05 +0300

    onChange

--- react/notes/rc06_forms-otherEvents/src/components/1-forms/Forms.jsx
+++ react/notes/rc06_forms-otherEvents/src/components/1-forms/Forms.jsx
@@ -1,30 +1,43 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
 
 const Forms = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleUsername = () => {}
+
   return (
-  <div>
+  <Container className='mt-4'>
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <h1 className='text-success'>FORMS</h1>
+
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label>Hello</Form.Label>
+        <Form.Control 
+        type="text" 
+        placeholder="Username" 
+        onChange={handleUsername} />        
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email:</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="password">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
+      
+      <Button variant="success" type="submit">
         Submit
       </Button>
     </Form>
-  </div>
+  </Container>
   )
 }
 
