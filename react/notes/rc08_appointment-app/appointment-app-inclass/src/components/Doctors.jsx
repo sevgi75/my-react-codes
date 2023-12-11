35089d81dcc853756329300f153b578b91919638
sevgi75 <erbilsevgi97@gmail.com>
2023-12-11 20:36:37 +0300

    doctors row col

--- react/notes/rc08_appointment-app/appointment-app-inclass/src/components/Doctors.jsx
+++ react/notes/rc08_appointment-app/appointment-app-inclass/src/components/Doctors.jsx
@@ -1,4 +1,7 @@
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { doctorData } from "../helper/data";
 
 const Doctors = () => {
   return (
@@ -6,6 +9,15 @@ const Doctors = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row>
+        {doctorData.map(({ id, img, dep, name }) => (
+          <Col key={id}>
+            <img src={img} alt={name} className="img-thumbnail doctor-img " />
+            <h5>{name}</h5>
+            <h6>{dep} </h6>
+          </Col>
+        ))}
+      </Row>
     
     </Container>
   )
