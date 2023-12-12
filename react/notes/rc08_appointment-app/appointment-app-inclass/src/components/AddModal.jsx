170d3970134f16227b862ca4d2dfba99786f2279
sevgi75 <erbilsevgi97@gmail.com>
2023-12-12 17:06:26 +0300

    use state

--- react/notes/rc08_appointment-app/appointment-app-inclass/src/components/AddModal.jsx
+++ react/notes/rc08_appointment-app/appointment-app-inclass/src/components/AddModal.jsx
@@ -1,31 +1,39 @@
-
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function AddModal({show, handleClose, drName}) {
-  
+    const [name, setName] = useState("")
+    const [date, setDate] = useState("")
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, date);
+    }
 
   return (
-    <>
-      
+    <>     
 
       <Modal show={show} onHide={handleClose}>
+
         <Modal.Header closeButton>
           <Modal.Title>Appointment for {drName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form>
+
+        <Form onSubmit={handleSubmit}>
+
       <Form.Group className="mb-3" controlId="name">
         <Form.Label>Patient Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" required />
-        
+        <Form.Control onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter name" required />      
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="date">
         <Form.Label>Date</Form.Label>
-        <Form.Control type="datetime-local" placeholder="Date" required />
+        <Form.Control onChange={(e) => setDate(e.target.value)} type="datetime-local" placeholder="Date" required />
       </Form.Group>
+
       <div className='text-center'>
           <Button variant="success" type='submit' className='me-2'>
             Save
--- react/notes/rc08_appointment-app/appointment-app-inclass/src/pages/Home.jsx
+++ react/notes/rc08_appointment-app/appointment-app-inclass/src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import AppointmentList from "../components/AppointmentList"
-import Doctors from "../components/Doctors"
+import { useState } from "react";
+import AppointmentList from "../components/AppointmentList";
+import Doctors from "../components/Doctors";
+import { appointmentData } from "../helper/data";
 
 const Home = () => {
+  const [appointments, setAppointments] = useState(appointmentData)
   return (
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
       <Doctors />
-      <AppointmentList />
+      <AppointmentList appointments={appointments} />
     </main>
   )
 }
