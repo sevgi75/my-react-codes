55be085bb23322c61c8771e7883774cf20411bd8
sevgi75 <erbilsevgi97@gmail.com>
2023-12-12 18:42:02 +0300

    local storage

--- react/notes/rc08_appointment-app/appointment-app-inclass/src/pages/Home.jsx
+++ react/notes/rc08_appointment-app/appointment-app-inclass/src/pages/Home.jsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import AppointmentList from "../components/AppointmentList";
 import Doctors from "../components/Doctors";
-import { appointmentData } from "../helper/data";
+// import { appointmentData } from "../helper/data";
 
 const Home = () => {
-  const [appointments, setAppointments] = useState(appointmentData);
+  const [appointments, setAppointments] = useState(JSON.parse(localStorage.getItem("list")) || []);
+  // const [appointments, setAppointments] = useState(appointmentData);
 
   const handleAdd = (newAppointment) => {
-    setAppointments([...appointments, newAppointment])
+    setAppointments([...appointments, newAppointment]);
+    localStorage.setItem("list", JSON.stringify([...appointments, newAppointment]))
   };
 
   const handleDelete = (id) => {
     const filteredList = appointments.filter(item => item.id !== id)
-    setAppointments(filteredList)
+    setAppointments(filteredList);
+    localStorage.setItem("list", JSON.stringify(filteredList))
   };
 
   const handleDoubleClick = (id) => {
     const updatedList = appointments.map(item => item.id === id ? {...item, consulted: !item.consulted} : item)
     setAppointments(updatedList);
+    localStorage.setItem("list", JSON.stringify(updatedList))
     // console.log(id);
   }
   return (
