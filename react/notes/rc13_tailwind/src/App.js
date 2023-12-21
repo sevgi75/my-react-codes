eee921058c9b643c2d304e00e8cbc52379e051d8
sevgi75 <erbilsevgi97@gmail.com>
2023-12-21 15:53:35 +0300

    spacing and sizing

--- react/notes/rc13_tailwind/src/App.js
+++ react/notes/rc13_tailwind/src/App.js
@@ -1,10 +1,12 @@
-import Typografy from "./components/Typografy";
+import SpacingAndSizing from "./components/SpacingAndSizing";
+// import Typografy from "./components/Typografy";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1>
-      <Typografy />
+      {/* <Typografy /> */}
+      <SpacingAndSizing />
     </div>
   );
 }
