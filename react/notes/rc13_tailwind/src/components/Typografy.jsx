0909a5cce329fd4fdb1bd2559599b866c514c1f4
sevgi75 <erbilsevgi97@gmail.com>
2023-12-21 15:30:45 +0300

    typografy color

--- react/notes/rc13_tailwind/src/components/Typografy.jsx
+++ react/notes/rc13_tailwind/src/components/Typografy.jsx
@@ -11,6 +11,20 @@ const Typografy = () => {
         <p className="text-2xl">Typografy</p>
         <h2 className="text-3xl">Başlık</h2>
         <h1 className="text-[60px]">Başlık</h1>
+        <article>
+            <p className="line-clamp-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsum nam fugiat, similique perspiciatis quas voluptas in ipsam consequuntur accusamus! Omnis nulla praesentium esse optio totam tenetur sit animi temporibus dicta minus nostrum, pariatur quis quibusdam laboriosam facilis quam blanditiis! Voluptate consectetur omnis, ex tempora culpa vero praesentium, sapiente impedit voluptatem id eius officia adipisci rerum! Eligendi numquam, perspiciatis quas, sed error fuga autem temporibus vel nulla possimus minus delectus distinctio vitae expedita nemo inventore sint. Velit inventore, sed ab maxime veritatis repellat dignissimos voluptatum ad commodi at, sequi consectetur aliquam qui exercitationem unde officiis explicabo obcaecati. Dolores, quisquam nemo!
+            </p>
+        </article>
+        <p className="text-center">Merhaba</p>
+        <p className="underline">merhaba</p>
+        <p className="line-through">Merhaba</p>
+        <p className="uppercase">Merhaba</p>
+        <p className="lowercase">Merhaba</p>
+        <p className="text-red-400">Merhaba</p>
+        <p className="text-[#6c97a7]">Merhaba</p>
+        <p className="text-[rgb(108,151,167)]">Merhaba</p>
+        <p className="text-[rgba(218,182,182,0.996)]">Merhaba</p>
     </div>
   )
 }
