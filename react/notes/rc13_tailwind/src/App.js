ef8c14a3a7a6d7843d96b1e24e712ff65eeca430
sevgi75 <erbilsevgi97@gmail.com>
2023-12-21 15:10:25 +0300

    typografy font size

--- react/notes/rc13_tailwind/src/App.js
+++ react/notes/rc13_tailwind/src/App.js
@@ -1,7 +1,10 @@
+import Typografy from "./components/Typografy";
+
 function App() {
   return (
     <div className="App">
       <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1>
+      <Typografy />
     </div>
   );
 }
---
+++ react/notes/rc13_tailwind/src/components/Typografy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Typografy = () => {
+  return (
+    <div>
+        <h3 className="text-2xl">Typografy</h3>
+        <p className="text-sm">Typografy</p>
+        <p className="text-base/7">Typografy</p>
+        <p className="text-lg">Typografy</p>
+        <p className="text-xl">Typografy</p>
+        <p className="text-2xl">Typografy</p>
+        <h2 className="text-3xl">Başlık</h2>
+        <h1 className="text-[60px]">Başlık</h1>
+    </div>
+  )
+}
+
+export default Typografy
