f9b5e33c5c97e708c3e990c2b88f3df2acf3d456
sevgi75 <erbilsevgi97@gmail.com>
2023-12-22 20:23:25 +0300

    width

--- react/notes/rc13_tailwind/src/components/SpacingAndSizing.jsx
+++ react/notes/rc13_tailwind/src/components/SpacingAndSizing.jsx
@@ -15,6 +15,12 @@ const SpacingAndSizing = () => {
         <button className="bg-green-500 text-white mx-4 px-4 py-2 rounded-[10px] border-[3px] border-orange-500">Tıkla</button>
         <button className="bg-green-500 text-white mx-4 px-4 py-4 rounded-full">Tıkla</button>
     </div>
+    <div>
+      <button className="w-20 bg-indigo-300 rounded-full py-2 mx-1">Tıkla</button>
+      <button className="w-1/2 bg-indigo-300 rounded-full py-2 mx-1">Tıkla</button>
+      <button className="w-[300px] bg-indigo-300 rounded-full py-2 mx-1">Tıkla</button>
+      <button className="max-w-lg min-w-[100px] w-2/3 bg-indigo-300 rounded-full py-2 mx-1">Tıkla</button>
+    </div>
     </>
   )
 }
