29914f6f92e5370f51c0b4a5be1d866b1d4c21fc
sevgi75 <erbilsevgi97@gmail.com>
2023-12-24 22:48:56 +0300

    tailwind finished

--- react/notes/rc13_tailwind/src/App.js
+++ react/notes/rc13_tailwind/src/App.js
@@ -1,5 +1,6 @@
 // import FlexAndResponsive from "./components/FlexAndResponsive";
 import Navbar from "./components/Navbar";
+import Profile from "./pages/Profile";
 // import HoverFocus from "./components/HoverFocus";
 // import SpacingAndSizing from "./components/SpacingAndSizing";
 // import Typografy from "./components/Typografy";
@@ -8,11 +9,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1>
+      {/* <h1 className="text-2xl text-center text-purple-700 font-bold">Hello Tailwind</h1> */}
       {/* <Typografy /> */}
       {/* <SpacingAndSizing /> */}
       {/* <HoverFocus /> */}
       {/* <FlexAndResponsive /> */}
+      <Profile />
     </div>
   );
 }
