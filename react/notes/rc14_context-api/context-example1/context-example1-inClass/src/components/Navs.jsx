fc8951b87fa6b2e75730fd47be6786ff0d2a7b6c
sevgi75 <erbilsevgi97@gmail.com>
2023-12-25 13:21:15 +0300

    navs

--- react/notes/rc14_context-api/context-example1/context-example1-inClass/src/components/Navs.jsx
+++ react/notes/rc14_context-api/context-example1/context-example1-inClass/src/components/Navs.jsx
@@ -3,8 +3,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
+import { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 function Navs() {
+  const {user, setUser} = useContext(LoginContext)
   return (
     <Navbar expand="md">
       <Container>
@@ -29,9 +32,13 @@ function Navs() {
             <Link className="nav-link" to="/people">
               People
             </Link>
-            <Link className="nav-link" to="/login">
+
+            {user.email && user.password ? (<Link className="nav-link" to="/login" onClick={() => setUser({email:"", password:""})}>
+              Logout
+            </Link>) :(<Link className="nav-link" to="/login">
               Login
-            </Link>
+            </Link>) }
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
