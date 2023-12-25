46f1bb62d59c9d3d5682bf1f09a303e65355f1e6
sevgi75 <erbilsevgi97@gmail.com>
2023-12-25 17:10:43 +0300

    useReducerExample

--- react/notes/rc14_context-api/reducer-example/reducer-inClass/src/UseReducerExample.js
+++ react/notes/rc14_context-api/reducer-example/reducer-inClass/src/UseReducerExample.js
@@ -1,40 +1,38 @@
-import { useState, useReducer } from "react";
-import { reducer } from "./reducer";
-import { initialState } from "./reducer";
+import { useState, useReducer } from "react"
+import { reducer } from "./reducer"
+import { initialState } from "./reducer"
 
 const UseStateExample = () => {
-  // const [catImage, setCatImage] = useState("");
-  // const [error, setError] = useState("");
-  // const [loading, setLoading] = useState(false);
+  // const [catImage, setCatImage] = useState("")
+  // const [error, setError] = useState("")
+  // const [loading, setLoading] = useState(false)
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  const {loading, error, catImage} = state
+  const { loading, error, catImage } = state
 
   const getCatImage = async () => {
-    const url = "https://api.thecatapi.com/v1/images/search";
-    // setLoading(true);
+    const url = "https://api.thecatapi.com/v1/images/search"
+    // setLoading(true)
     //? 1. istegin basladigi asama (START)
-    dispatch({type:"START", payload:""})
+    dispatch({ type: "START", payload: "" })
     try {
-      const res = await fetch(url);
-      const data = await res.json();
-      // setCatImage(data[0].url);
-      // setError("");
+      const res = await fetch(url)
+      const data = await res.json()
+      // setCatImage(data[0].url)
+      // setError("")
       //? 2. istegin basarili bittigi asama
-      dispatch({type:"SUCCESS", payload:data[0].url})
+      dispatch({ type: "SUCCESS", payload: data[0].url })
     } catch (error) {
-      // setError("DATA CAN NOT BE FETCHED");
-      // setCatImage("");
+      // setError("DATA CAN NOT BE FETCHED")
+      // setCatImage("")
       //? 3. istegin basarisiz bittigi asama
-      dispatch({type:"FAIL", payload: "DATA CAN NOT BE FETCHED"})
-      console.log(error);
-    } finally {
-      // setLoading(false);
+      dispatch({ type: "FAIL", payload: "DATA CAN NOT BE FETCHED" })
+      console.log(error)
     }
-  };
-  console.log(error);
-  console.log(catImage);
+  }
+  console.log(error)
+  console.log(catImage)
 
   return (
     <div>
@@ -48,7 +46,7 @@ const UseStateExample = () => {
       {error && <h2>{error}</h2>}
       {catImage && <img width="50%" src={catImage} alt="img" />}
     </div>
-  );
-};
+  )
+}
 
-export default UseStateExample;
+export default UseStateExample
--- react/notes/rc14_context-api/reducer-example/reducer-inClass/src/reducer.js
+++ react/notes/rc14_context-api/reducer-example/reducer-inClass/src/reducer.js
@@ -2,29 +2,28 @@ export const initialState = {
     loading: false,
     catImage: "",
     error: "",
-}
-
-export const reducer = (initialState, action) => {
+  }
+  
+  export const reducer = (initialState, action) => {
     switch (action.type) {
-        case "START":
-            return {...initialState, loading: true}            
-        case "SUCCESS":
-            return {
-                ...initialState, 
-                catImage: action.payload, 
-                loading: false, 
-                error: "",
-            }            
-        case "FAIL":
-            return {
-                ...initialState, 
-                catImage: "", 
-                loading: false, 
-                error: action.payload,
-            }            
-            
-    
-        default:
-            break;
+      case "START":
+        return { ...initialState, loading: true }
+      case "SUCCESS":
+        return {
+          ...initialState,
+          catImage: action.payload,
+          loading: false,
+          error: "",
+        }
+      case "FAIL":
+        return {
+          ...initialState,
+          catImage: "",
+          loading: false,
+          error: action.payload,
+        }
+      default:
+        return initialState
     }
-}
+  }
+  
