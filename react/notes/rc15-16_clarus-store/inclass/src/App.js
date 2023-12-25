d9d3080a7df161295106327657e50e79ddc3eb7e
sevgi75 <erbilsevgi97@gmail.com>
2023-12-25 20:02:39 +0300

    login

--- react/notes/rc15-16_clarus-store/inclass/src/App.js
+++ react/notes/rc15-16_clarus-store/inclass/src/App.js
@@ -1,8 +1,12 @@
+import AuthProvider from "./context/AuthProvider";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <AppRouter />
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+    
   );
 }
 
