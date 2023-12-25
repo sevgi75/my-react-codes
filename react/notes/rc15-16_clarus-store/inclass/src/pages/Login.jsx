0ee703ddee1fe34ab25d87f169b74c58a9f78eef
sevgi75 <erbilsevgi97@gmail.com>
2023-12-25 19:30:55 +0300

    login

--- react/notes/rc15-16_clarus-store/inclass/src/pages/Login.jsx
+++ react/notes/rc15-16_clarus-store/inclass/src/pages/Login.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Login = () => {
   return (
     <div className="loginDiv">
-      <div>
-        <div>
-          <span></span>
-          <h1>Clarus Store</h1>
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex justify-between flex-col items-center">
+        <div className="flex justify-center items-center mt-2 gap-2">
+          <span className="w-[6px] bg-yellow-500 h-[39px]"></span>
+          <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase">Clarus Store</h1>
         </div>
         <div>
           <h3>SIGN IN</h3>
