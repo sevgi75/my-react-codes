b7afaefb764af1a1656683145b819952546adceb
sevgi75 <erbilsevgi97@gmail.com>
2023-12-29 12:45:49 +0300

    redirects

--- react/notes/rc15-16_clarus-store/inclass/src/context/AuthProvider.jsx
+++ react/notes/rc15-16_clarus-store/inclass/src/context/AuthProvider.jsx
@@ -20,7 +20,7 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     sessionStorage.setItem("user", JSON.stringify(user))
-  },[user])
+  },[user])//! componentDidUpdate
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
--- react/notes/rc15-16_clarus-store/inclass/src/router/PrivateRouter.jsx
+++ react/notes/rc15-16_clarus-store/inclass/src/router/PrivateRouter.jsx
@@ -1,18 +1,37 @@
-import React, { useContext } from 'react'
-import Navbar from '../components/Navbar'
-import { Navigate, Outlet } from 'react-router-dom'
-import { AuthContext } from '../context/AuthProvider'
+import React, { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { AuthContext } from "../context/AuthProvider";
 
 const PrivateRouter = () => {
   const {user} = useContext(AuthContext)
   return user ? (
     <>
-    <Navbar />
-    <Outlet />
+      <Navbar />
+      <Outlet />
     </>
   ) : (
-    <Navigate />
-  )
-}
+    <Navigate to="/" />
+  );
+};
 
-export default PrivateRouter
+export default PrivateRouter;
+
+//! context kullanmadan user bilgilerini okuma
+// import React from "react";
+// import { Navigate, Outlet } from "react-router-dom";
+// import Navbar from "../components/Navbar";
+
+// const PrivateRouter = () => {
+//   const user = sessionStorage.getItem("user")
+//   return user ? (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//     </>
+//   ) : (
+//     <Navigate to="/" />
+//   );
+// };
+
+// export default PrivateRouter;
