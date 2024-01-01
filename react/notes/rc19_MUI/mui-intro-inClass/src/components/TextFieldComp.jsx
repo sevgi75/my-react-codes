94e787773646f84fc75d20d57af3fe394bb606b7
sevgi75 <erbilsevgi97@gmail.com>
2024-01-01 14:53:57 +0300

    textfieldcomp

--- react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
+++ react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
@@ -8,7 +8,7 @@ const TextFieldComp = () => {
     <Container maxWidth="md" sx={{mt: 4}} align="center">
         <TextField id="email" label="email" type="email" variant="outlined" placeholder="Enter your email" color="warning" required fullWidth margin="normal" error={err} helperText={err && "Yanlis email"} />
         <TextField id="password" type="password" label="password" variant="filled" fullWidth margin="normal" />
-        <TextField id="adress" label="adress" variant="standard" type="text" fullWidth margin="normal" />
+        <TextField id="adress" label="adress" variant="standard" type="text" fullWidth margin="normal" sx={{backgroundColor: "secondary.light"}} />
     </Container>
   )
 }
