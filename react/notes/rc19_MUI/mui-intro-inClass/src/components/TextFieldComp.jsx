05a7d34fc46136d1f6b7f3d73c77304a61c841e8
sevgi75 <erbilsevgi97@gmail.com>
2024-01-01 14:38:58 +0300

    textfield

--- react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
+++ react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
@@ -3,10 +3,10 @@ import TextField from "@mui/material/TextField"
 
 const TextFieldComp = () => {
   return (
-    <Container maxWidth="md" sx={{m: 4}} align="center">
-        <TextField id="email" label="email" variant="outlined" placeholder="Enter your email" color="warning" required fullWidth />
-        <TextField id="filled-basic" label="Filled" variant="filled" fullWidth />
-        <TextField id="standard-basic" label="Standard" variant="standard" fullWidth />
+    <Container maxWidth="md" sx={{mt: 4}} align="center">
+        <TextField id="email" label="email" type="email" variant="outlined" placeholder="Enter your email" color="warning" required fullWidth margin="normal" />
+        <TextField id="password" type="password" label="password" variant="filled" fullWidth margin="normal" />
+        <TextField id="adress" label="adress" variant="standard" type="text" fullWidth margin="normal" />
     </Container>
   )
 }
