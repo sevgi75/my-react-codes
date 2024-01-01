b5c46384a3447bd2be64ce31cc0bc78ac01e80d5
sevgi75 <erbilsevgi97@gmail.com>
2024-01-01 14:50:16 +0300

    textfield

--- react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
+++ react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
@@ -1,10 +1,12 @@
 import Container from "@mui/material/Container"
 import TextField from "@mui/material/TextField"
+import { useState } from "react"
 
 const TextFieldComp = () => {
+    const [err, setErr] = useState(true)
   return (
     <Container maxWidth="md" sx={{mt: 4}} align="center">
-        <TextField id="email" label="email" type="email" variant="outlined" placeholder="Enter your email" color="warning" required fullWidth margin="normal" />
+        <TextField id="email" label="email" type="email" variant="outlined" placeholder="Enter your email" color="warning" required fullWidth margin="normal" error={err} helperText={err && "Yanlis email"} />
         <TextField id="password" type="password" label="password" variant="filled" fullWidth margin="normal" />
         <TextField id="adress" label="adress" variant="standard" type="text" fullWidth margin="normal" />
     </Container>
