7101247ac839e003cd93d7b7acbecf21ae879fa0
sevgi75 <erbilsevgi97@gmail.com>
2024-01-01 14:21:31 +0300

    text field

---
+++ react/notes/rc19_MUI/mui-intro-inClass/src/components/TextFieldComp.jsx
@@ -0,0 +1,13 @@
+import TextField from "@mui/material/TextField"
+
+const TextFieldComp = () => {
+  return (
+    <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+    </div>
+  )
+}
+
+export default TextFieldComp
