cc7555e8d813248adcdb370bf7fd344d90f17445
sevgi75 <erbilsevgi97@gmail.com>
2024-01-01 18:09:47 +0300

    navbar

--- react/notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
+++ react/notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
@@ -4,9 +4,11 @@ import Main from "../pages/Main";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
   return <BrowserRouter>
+  <Navbar />
   <Routes>
     <Route path="/" element={<Main />} />
     <Route path="/login" element={<Login />} />
