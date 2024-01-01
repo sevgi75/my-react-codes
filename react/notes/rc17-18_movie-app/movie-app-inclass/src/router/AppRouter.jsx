4a1c2f2d5773c92cfca15d644b80a87efa6cca8b
sevgi75 <erbilsevgi97@gmail.com>
2024-01-01 17:27:19 +0300

    AppRouter

--- react/notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
+++ react/notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Main />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/details/:id" element={<MovieDetail />} />
+  </Routes>
+  </BrowserRouter>;
 };
 
 export default AppRouter;
