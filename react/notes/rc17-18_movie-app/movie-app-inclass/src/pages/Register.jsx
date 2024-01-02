9a15e4817fb5b9e093908e83832851bdfa4f2d9f
sevgi75 <erbilsevgi97@gmail.com>
2024-01-03 00:13:31 +0300

    info setInfo

--- react/notes/rc17-18_movie-app/movie-app-inclass/src/pages/Register.jsx
+++ react/notes/rc17-18_movie-app/movie-app-inclass/src/pages/Register.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
 
 const Register = () => {
+  //* ayri stateler
+  // const [firstName, setFirstName] = useState();
+  // const [lastName, setLastName] = useState();
+  // const [email, setEmail] = useState();
+  // const [password, setPassword] = useState();
+  //! inputlarla calisiyorsak statelerin ilk degeri null veya undefined olmamali
+  const [info, setInfo] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:""
+  });
+  const handleChange = (e) => 
+  setInfo({...info, [e.target.name]: e.target.value});
+  console.log(info);
   return (
     <div className="flex justify-center">
       <div className="overflow-hidden flex-1 h-screen justify-center items-center dark:bg-gray-dark-main">
@@ -12,41 +27,45 @@ const Register = () => {
             </h2>
             <div className="relative z-0 w-full mb-6 group">
               <input
-                name="floating_text"
+                name="firstName"
                 className="peer"
                 type="text"
                 required
                 placeholder=" "
+                onChange={handleChange}
               />
               <label htmlFor="floating_text">First Name</label>
             </div>
             <div className="relative z-0 w-full mb-6 group">
               <input
-                name="floating_text"
+                name="lastName"
                 className="peer"
                 type="text"
                 required
                 placeholder=" "
+                onChange={handleChange}
               />
               <label htmlFor="floating_text">Last Name</label>
             </div>
             <div className="relative z-0 w-full mb-6 group">
               <input
-                name="floating_email"
+                name="email"
                 className="peer"
                 type="email"
                 placeholder=" "
                 required
+                onChange={handleChange}
               />
               <label htmlFor="floating_email">Email</label>
             </div>
             <div className="relative z-0 w-full mb-6 group">
               <input
-                name="floating_password"
+                name="password"
                 className="peer"
                 type="password"
                 placeholder=" "
                 required
+                onChange={handleChange}
               />
               <label htmlFor="floating_password">Password</label>
             </div>
