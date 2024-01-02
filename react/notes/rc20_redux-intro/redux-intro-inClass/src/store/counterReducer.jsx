e6e0426c1df2aab3436a53693551bf1be9616fa3
sevgi75 <erbilsevgi97@gmail.com>
2024-01-02 21:51:13 +0300

    counter

--- react/notes/rc20_redux-intro/redux-intro-inClass/src/store/counterReducer.jsx
+++ react/notes/rc20_redux-intro/redux-intro-inClass/src/store/counterReducer.jsx
@@ -1,16 +1,20 @@
 const initialState = {
-    count: 0,
+    count: 10,
 }
+//? type larin CONSTANT olarak tanimlanmasi
+export const INC = 'INC'
+export const DEC = 'DEC'
+export const CLR = 'CLR'
 
 export const counterReducer = (state=initialState, action) => {
     switch (action.type) {
-        case "INC" :
+        case INC :
             return {count: state.count + 1}
         
-        case "DEC" :
+        case DEC :
             return {count: state.count - 1}
             
-        case "CLR" :
+        case CLR :
             return {count: 0}
             
         default :
