13cef56a9d9a243c483387bcd569e6e1ab36233c
sevgi75 <erbilsevgi97@gmail.com>
2024-01-02 20:53:04 +0300

    redux state

---
+++ react/notes/rc20_redux-intro/redux-intro-inClass/src/store/counterReducer.jsx
@@ -0,0 +1,19 @@
+const initialState = {
+    count: 0,
+}
+
+const counterReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case "INC" :
+            return {count: state.count + 1}
+        
+        case "DEC" :
+            return {count: state.count - 1}
+            
+        case "CLR" :
+            return {count: 0}
+            
+        default :
+            return state    
+    }
+}
