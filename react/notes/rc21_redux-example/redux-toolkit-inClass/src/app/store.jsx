153e058c019c6946a24aa8ab39e252811b0eda0b
sevgi75 <erbilsevgi97@gmail.com>
2024-01-04 21:34:53 +0300

    dev tools

--- react/notes/rc21_redux-example/redux-toolkit-inClass/src/app/store.jsx
+++ react/notes/rc21_redux-example/redux-toolkit-inClass/src/app/store.jsx
@@ -6,5 +6,7 @@ export const store = configureStore({
     reducer:{
         auth:authReducer,
         newsApi: newsReducer,
-    }
+    },
+    devTools: process.env.NODE_ENV !== "production",
+  //? eger gelistirme asamasi production ise o zaman yukaridaki ifade false dondurur ve dolayisiyla devTool kullanima kapali olur.
 })
