366fff39f16dbc1d37bddfa3edfaf1c4c8dd20a1
sevgi75 <erbilsevgi97@gmail.com>
2024-01-08 15:02:49 +0300

    authSlice

--- react/notes/rc22-26_stock-app/stock-app-inClass1/src/service/useAuthCalls.jsx
+++ react/notes/rc22-26_stock-app/stock-app-inClass1/src/service/useAuthCalls.jsx
@@ -1,17 +1,23 @@
 import axios from "axios"
 import {toastErrorNotify, toastSuccessNotify} from "../helper/ToastNotify"
 import { useNavigate } from "react-router-dom"
+import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice"
+import {useDispatch} from "react-redux"
 
 const useAuthCalls = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const login = async (userInfo) => {
+        dispatch(fetchStart())
     try {
         const {data} = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login/`, userInfo)
+        dispatch(loginSuccess(data))
         toastSuccessNotify("Login işlemi başarili.")
         navigate("/stock")
-        console.log(data);
+        
     } catch (error) {
+        dispatch(fetchFail())
         toastErrorNotify("Login işlemi başarisiz oldu.")
         console.log(error);
     }
