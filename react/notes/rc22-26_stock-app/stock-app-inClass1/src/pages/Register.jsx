0aec6b6bf88f3389629bd710a5c401b0c68d96c6
sevgi75 <erbilsevgi97@gmail.com>
2024-01-09 22:24:06 +0300

    feat:add Register

--- react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Register.jsx
+++ react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Register.jsx
@@ -6,12 +6,15 @@ import image from "../assets/result.svg"
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import TextField from "@mui/material/TextField"
+import { Formik } from "formik"
+import RegisterForm, { registerSchema } from "../components/RegisterForm"
+import useAuthCalls from "../service/useAuthCalls"
 
 const Register = () => {
-  const navigate = useNavigate()
+  const {register} = useAuthCalls()
 
   return (
     <Container maxWidth="lg">
@@ -51,49 +54,22 @@ const Register = () => {
             Register
           </Typography>
 
-          <Box
-            component="form"
-            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-          >
-            <TextField
-              label="User Name"
-              name="username"
-              id="userName"
-              type="text"
-              variant="outlined"
-            />
-            <TextField
-              label="First Name"
-              name="first_name"
-              id="firstName"
-              type="text"
-              variant="outlined"
-            />
-            <TextField
-              label="Last Name"
-              name="last_name"
-              id="last_name"
-              type="text"
-              variant="outlined"
-            />
-            <TextField
-              label="Email"
-              name="email"
-              id="email"
-              type="email"
-              variant="outlined"
-            />
-            <TextField
-              label="password"
-              name="password"
-              id="password"
-              type="password"
-              variant="outlined"
-            />
-            <Button type="submit" variant="contained" size="large">
-              Submit
-            </Button>
-          </Box>
+          <Formik
+          initialValues={{
+            username:"",
+            firstName:"",
+            lastName:"",
+            email:"",
+            password:"",
+          }}
+          validationSchema={registerSchema}
+          onSubmit={(values, actions) => {
+            register(values)
+            actions.RegisterForm()
+            actions.setSubmitting(false)
+          }}
+           component={(props) => <RegisterForm {...props} />}
+          ></Formik>
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/">Do you have an account?</Link>
