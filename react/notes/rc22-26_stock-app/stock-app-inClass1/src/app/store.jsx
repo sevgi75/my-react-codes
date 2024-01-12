2ce0d4a8f0729b8534ff3de489fd7a570ae20bac
sevgi75 <erbilsevgi97@gmail.com>
2024-01-12 16:05:32 +0300

    fix:serialisable error

--- react/notes/rc22-26_stock-app/stock-app-inClass1/src/app/store.jsx
+++ react/notes/rc22-26_stock-app/stock-app-inClass1/src/app/store.jsx
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, 
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //? local storage
+// import storage from 'redux-persist/lib/storage/session' //? session storage
 
 const persistConfig = {
   key: 'root',
@@ -16,6 +26,12 @@ const store = configureStore({
     // stock: stockReducer
   },
   devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export const persistor = persistStore(store)
 
