c7c2137cf2e7dd002edc5eb4aae05115031056fe
sevgi75 <erbilsevgi97@gmail.com>
2024-01-12 20:24:14 +0300

    feat:add FirmModal

--- react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Firms.jsx
+++ react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Firms.jsx
@@ -5,6 +5,7 @@ import useStockCalls from "../service/useStockCalls";
 import { useSelector } from "react-redux";
 import { Grid } from "@mui/material";
 import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/FirmModal";
 
 
 const Firms = () => {
@@ -26,7 +27,7 @@ const Firms = () => {
       <div>
         <Typography variant="h4" color="error">Firms</Typography>
         <Button variant="contained">New Firm</Button>
-
+        <FirmModal />
         <Grid container gap={2} mt={3} justifyContent={"center"}>
           {firms?.map((firm) => (
             <Grid item key={firm._id}>
