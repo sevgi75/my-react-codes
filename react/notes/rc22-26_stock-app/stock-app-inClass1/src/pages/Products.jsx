4cb1a3357df4d5f1b84cd19de8114a7fbdf5a791
sevgi75 <erbilsevgi97@gmail.com>
2024-01-17 15:05:19 +0300

    product modal

--- react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Products.jsx
+++ react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Products.jsx
@@ -1,7 +1,54 @@
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import useStockCalls from "../service/useStockCalls";
+import { useSelector } from "react-redux";
+import { Grid } from "@mui/material";
+import ProductModal from "../components/ProductModal";
+
 
 const Products = () => {
+  // const {getFirms, getSales} = useStockCalls()
+  const {getStocks} = useStockCalls()
+  const {firms} = useSelector((state) => state.stock)
+
+  const [info, setInfo] = useState({
+    "name": "",
+    "phone": "",
+    "address": "",
+    "image": "",
+  })
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false)
+    setInfo({name: "", phone: "", address: "", image: ""})
+  };
+
+  useEffect(() => {
+    // getFirms()
+    // getSales()
+    getStocks("firms")
+    
+    
+  }, [])
+
+  console.log(firms);
+  
     return (
-      <div>Products</div>
+      <div>
+        <Typography variant="h4" color="error">Firms</Typography>
+        <Button variant="contained" onClick={handleOpen}>New Firm</Button>
+        <ProductModal 
+        open={open} 
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo} 
+        />
+        
+        
+      </div>
     )
   }
   
