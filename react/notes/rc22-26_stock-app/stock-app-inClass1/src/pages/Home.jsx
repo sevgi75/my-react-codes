cb16303968ec34d41bb2835f121233815d4ab5c8
sevgi75 <erbilsevgi97@gmail.com>
2024-01-19 23:52:33 +0300

    kpiCard

--- react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Home.jsx
+++ react/notes/rc22-26_stock-app/stock-app-inClass1/src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react"
 import Charts from "../components/Charts"
 import KPI from "../components/KPI"
+import useStockCalls from "../service/useStockCalls"
 
 const Home = () => {
+  const {getStocks} = useStockCalls()
+
+  useEffect(() => {
+    getStocks("sales")
+    getStocks("purchases")
+  },[])
     return (
       <>
         <KPI />
