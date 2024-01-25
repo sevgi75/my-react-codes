1b860ff11d57ceb13b2f356c89faf4d9ce96eb1a
sevgi75 <erbilsevgi97@gmail.com>
2024-01-25 16:07:17 +0300

    tuples

--- typescript/class-notes/S01 - Intro/out/app.js
+++ typescript/class-notes/S01 - Intro/out/app.js
@@ -1,5 +1,9 @@
-"use strict";
-console.log("Hello World");
-const add = (a, b) => a + b;
-// Merhaba
-// Dünya
+let myTuple;
+myTuple = [1, "Sevgi"];
+console.log(myTuple);
+myTuple.push(false);
+myTuple.push("Hello");
+let arrTuple = [];
+arrTuple.push([1, "Sevgi"]);
+arrTuple.push([2, "Enes"]);
+console.log(arrTuple);
--- typescript/class-notes/S01 - Intro/src/app.ts
+++ typescript/class-notes/S01 - Intro/src/app.ts
@@ -1,9 +1,63 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 
 
-const add = (a:any, b:number) => a+b
+// const add = (a:any, b:number) => a+b
 
 // Merhaba
 
 // Dünya
+
+//& Type annotations & Basic Types
+// let x: number = 5
+
+// x = 25
+
+// let yy = "Dünya"
+// let y: string = `Hello ${yy}`
+
+// yy = 5
+
+// let z: boolean = true
+
+// let a: number
+
+// let b;
+
+//& Arrays
+
+// let arr:number[] = [1,2,3,4]
+
+// arr.push("a")
+
+//? Bos array tanimlamasi
+// let arr2: string[] = []
+
+// arr2.push("Hello")
+// arr2.push(false)
+
+// let arr3:any[] = []
+
+// arr3.push("Hello")
+// arr3.push(false)
+// arr3.push(5)
+
+//& Tuples
+
+let myTuple: [number,string]
+
+myTuple = [1, "Sevgi"]
+
+console.log(myTuple);
+
+myTuple.push(false)
+myTuple.push("Hello")
+
+//& Array of Tuples
+
+let arrTuple: [number,string][] = []
+
+arrTuple.push([1,"Sevgi"])
+arrTuple.push([2,"Enes"])
+
+console.log(arrTuple);
