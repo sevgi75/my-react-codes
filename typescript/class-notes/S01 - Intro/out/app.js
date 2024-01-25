2eca16b3ef46bb79959964648b82168e06f12d40
sevgi75 <erbilsevgi97@gmail.com>
2024-01-25 15:10:11 +0300

    kurulum tamamlandi

---
+++ typescript/class-notes/S01 - Intro/out/app.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello World");
+const add = (a, b) => a + b;
+// Merhaba
+// Dünya
---
+++ typescript/class-notes/S01 - Intro/src/app.ts
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+
+
+const add = (a:any, b:number) => a+b
+
+// Merhaba
+
+// Dünya
