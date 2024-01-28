a9354f0e6457199e0b4685cdbc4e8c4db11e2184
sevgi75 <erbilsevgi97@gmail.com>
2024-01-29 00:14:39 +0300

    inheritance

--- typescript/class-notes/S02/dist/app.js
+++ typescript/class-notes/S02/dist/app.js
@@ -1,11 +1,34 @@
 //& Classes
-class Employee {
-    constructor(code, name) {
-        this.empCode = code;
-        this.empName = name;
+// class Employee {
+//     empCode: number;
+//     empName: string;
+//     constructor (code:number, name:string){
+//         this.empCode = code;
+//         this.empName = name;
+//     }
+// }
+// let emp1 = new Employee(1, "Sevgi")
+// // let emp2 = new Employee("Sevgi",2)
+// let emp3 = new Employee(2, "Ebrar")
+// console.log(emp3.empCode);
+//& Inheritance
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet() {
+        console.log(`Merhaba ${this.name}`);
     }
 }
-let emp1 = new Employee(1, "Sevgi");
-// let emp2 = new Employee("Sevgi",2)
-let emp3 = new Employee(2, "Ebrar");
-console.log(emp3.empCode);
+class Employee extends Person {
+    constructor(emplyCode, name) {
+        super(name);
+        this.emplyCode = emplyCode;
+    }
+}
+let person1 = new Person("Sevgi");
+console.log(person1.name);
+let emp4 = new Employee(100, "Enes");
+console.log(emp4.name);
+person1.greet();
+emp4.greet();
