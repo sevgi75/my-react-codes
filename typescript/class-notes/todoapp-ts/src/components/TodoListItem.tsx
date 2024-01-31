aa3d16736f90610d3827a24cdadf969f8ba3a49b
sevgi75 <erbilsevgi97@gmail.com>
2024-01-31 14:48:01 +0300

    todoapp

--- typescript/class-notes/todoapp-ts/src/components/TodoListItem.tsx
+++ typescript/class-notes/todoapp-ts/src/components/TodoListItem.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { FC } from "react";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ListItemText from "@mui/material/ListItemText";
 
-const TodoListItem = () => {
-  return (
-    <div>TodoListItem</div>
-  )
+interface ITodoListItem {
+  todo: ITodoType;
+  toggleTodo: ToggleFn;
+  deleteTodo: DeleteFn;
 }
 
-export default TodoListItem
+const TodoListItem: FC<ITodoListItem> = ({ todo, toggleTodo, deleteTodo }) => {
+  return (
+    <ListItem
+      disableGutters
+      sx={{ cursor: "pointer", padding: "1rem" }}
+      secondaryAction={
+        <IconButton sx={{ "&:hover": { color: "red" } }} aria-label="delete">
+          <DeleteIcon onClick={() => deleteTodo(todo.id)} />
+        </IconButton>
+      }
+    >
+      <ListItemText
+        onClick={() => toggleTodo(todo)}
+        primary={todo.task || todo.todo}
+        sx={{ wordWrap: "break-word" }}
+      />
+    </ListItem>
+  );
+};
+
+export default TodoListItem;
